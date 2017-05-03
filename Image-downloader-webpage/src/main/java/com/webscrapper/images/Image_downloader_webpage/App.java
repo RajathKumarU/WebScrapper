6d6fd502fba4ec1f23d1102626f26bce5e0593c8
Rajath Kumar <rkumar5@walmartlabs.com>
2017-05-03 11:20:55 +0530

    First Commit. Adding all the project files.

---
+++ Image-downloader-webpage/src/main/java/com/webscrapper/images/Image_downloader_webpage/App.java
@@ -0,0 +1,13 @@
+package com.webscrapper.images.Image_downloader_webpage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
