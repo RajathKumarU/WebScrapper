db0de010dd8343bf392a42ce39c75cc2ae5332f7
Rajath Kumar <rkumar5@walmartlabs.com>
2017-05-04 11:54:13 +0530

    Committing after completion of phase 1 to download wall papers above 100KB

---
+++ Image-downloader-webpage/src/main/java/com/webscrapper/images/scrapper/DownloadWallpaper.java
@@ -0,0 +1,17 @@
+package com.webscrapper.images.scrapper;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DownloadWallpaper {
+	public static void main(String[] args) throws IOException {
+		System.out.println("Enter/Paste a url from which wallpapers should be downloaded: ");
+		Scanner input = new Scanner(System.in);
+		String url = input.nextLine();
+
+		System.out.println("Find images downloaded in /src/main/java/downloads");
+		input.close();
+
+		WebScrapper.downloadImages(url);
+	}
+}
---
+++ Image-downloader-webpage/src/main/java/com/webscrapper/images/scrapper/WebScrapper.java
@@ -0,0 +1,102 @@
+package com.webscrapper.images.scrapper;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class WebScrapper {
+	public static void downloadImages(String urlString) throws IOException {
+
+		StringBuilder pageSource = getPageSource(urlString);
+		System.out.println("\n\nPageSource: \n" + pageSource + "\n");
+
+		List<String> imgLinks = getImageLinksFromSource(pageSource);
+		for (String link : imgLinks) {
+			System.out.println(link);
+		}
+
+		downloadImagesFromUrls(imgLinks);
+	}
+
+	public static void downloadImagesFromUrls(List<String> imgLinks) throws IOException {
+		for (String link : imgLinks) {
+			// Download image here
+			URL url = new URL(link);
+			String fileName = url.getFile();
+			String destName = "src/main/java/downloads" + fileName.substring(fileName.lastIndexOf("/"));
+			//System.out.println(destName);
+
+			InputStream is = url.openStream();
+			OutputStream os = new FileOutputStream(destName);
+
+			byte[] b = new byte[2048];
+			int length;
+			long fileSizeBytes = 0;
+
+			while ((length = is.read(b)) != -1) {
+				os.write(b, 0, length);
+				fileSizeBytes += length;
+			}
+
+			is.close();
+			if (fileSizeBytes > 100000)
+				os.close();
+		}
+	}
+
+	/* This method gets all the image links from a page source */
+	public static List<String> getImageLinksFromSource(StringBuilder pageSource) {
+		List<String> links = new LinkedList<String>();
+
+		StringBuilder sourceAfterFindingHttp = pageSource;
+		int indx = -1;
+		while ((indx = sourceAfterFindingHttp.indexOf("http")) != -1) {
+			String link = "";
+			int indxOfSingle = sourceAfterFindingHttp.indexOf("'", indx);
+			int indxOfDouble = sourceAfterFindingHttp.indexOf("\"", indx);
+
+			if (indxOfDouble == -1)
+				link = sourceAfterFindingHttp.substring(indx, indxOfSingle);
+			else if (indxOfSingle == -1)
+				link = sourceAfterFindingHttp.substring(indx, indxOfDouble);
+			else if ((indxOfSingle > indxOfDouble))
+				link = sourceAfterFindingHttp.substring(indx, indxOfDouble);
+			else if ((indxOfSingle < indxOfDouble))
+				link = sourceAfterFindingHttp.substring(indx, indxOfSingle);
+
+			String tempLink = link.toLowerCase();
+			if (tempLink.endsWith("jpg") || tempLink.endsWith("jpeg") || tempLink.endsWith("png")
+					|| tempLink.endsWith("bmp")) {
+				if (!links.contains(link))
+					links.add(link);
+			}
+
+			sourceAfterFindingHttp = new StringBuilder(sourceAfterFindingHttp.substring(indx + 1));
+		}
+
+		return links;
+	}
+
+	/* This method gets */
+	public static StringBuilder getPageSource(String urlString) throws IOException {
+		URL url = new URL(urlString);
+		URLConnection urlConn = url.openConnection();
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream(), "UTF-8"));
+		String line = "";
+		StringBuilder pageSrc = new StringBuilder();
+
+		while ((line = reader.readLine()) != null) {
+			pageSrc.append(line);
+		}
+
+		return pageSrc;
+	}
+}
